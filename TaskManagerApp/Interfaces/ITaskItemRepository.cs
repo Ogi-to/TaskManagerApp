4429d926b8a7b31712450132bc073d6930e687e8
Ognyan <konmagare115@gmail.com>
2026-06-07 10:14:34 +0300

    Idk

--- TaskManagerApp/Interfaces/ITaskItemRepository.cs
+++ TaskManagerApp/Interfaces/ITaskItemRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace TaskManagerApp.Interfaces
+﻿using TaskManagerApp.Data.Models;
+
+namespace TaskManagerApp.Interfaces
 {
     public interface ITaskItemRepository
     {
+        public TaskItem Get();
+
+        public TaskItem Update(TaskItem item);
     }
 }
--- TaskManagerApp/Interfaces/IUserRepository.cs
+++ TaskManagerApp/Interfaces/IUserRepository.cs
@@ -1,6 +1,21 @@
-﻿namespace TaskManagerApp.Interfaces
+﻿using TaskManagerApp.Data.Models;
+
+namespace TaskManagerApp.Interfaces
 {
     public interface IUserRepository
     {
+        public User Get();
+
+        public void CreateAccount();
+
+        public void UpdateAccount();
+
+        public void SendRequest();
+
+        public void RespondToRequest();
+
+        public void GetFriendsList();
+
+        public void GetFriendByName();
     }
 }
