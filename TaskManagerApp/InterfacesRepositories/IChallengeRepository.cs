17547e0083b98a65904669ea318db70d0ba35952
lora-peneva <lorapeneva8@outlook.com>
2026-07-07 18:16:07 +0300

    finished TaskItem and Challenge Services

--- TaskManagerApp/InterfacesRepositories/IChallengeRepository.cs
+++ TaskManagerApp/InterfacesRepositories/IChallengeRepository.cs
@@ -4,13 +4,15 @@ namespace TaskManagerApp.Interfaces
 {
     public interface IChallengeRepository
     {
-        public Task<Challenge> GetAsync(Challenge item);
+        public Task<Challenge> GetAsync(int challengeId);
         public Task<List<Challenge>> GetAllAsync();
-        public Task<List<Challenge>> GetChallengesByUserAsync(User item);
+        public Task<List<Challenge>> ShowAllByCategoryAsync(int categoryId);
+        public Task<List<Challenge>> ShowAllByLevelAsync(int levelNumber);
+        public Task<List<Challenge>> ShowAllCompletedByUserAsync(int userId);
+        public Task<List<Challenge>> GetChallengesByUserAsync(int userId);
 
         public Task JoinChallengeAsync(Challenge challenge, User user);
-
-        public Task CompleteChalengeAsync(Challenge challenge, User user);
+        public Task CompleteChallengeAsync(Challenge challenge, User user);
 
 
     }
---
+++ TaskManagerApp/Services/ChallengeService.cs
@@ -0,0 +1,141 @@
+﻿//using Org.BouncyCastle.Asn1.Cmp; nqmam predstawa otkyde doide
+using TaskManagerApp.Data.Models;
+using TaskManagerApp.Exceptions;
+using TaskManagerApp.InterfacesServices;
+using TaskManagerApp.Repositories;
+
+namespace TaskManagerApp.Services
+{
+    public class ChallengeService : IChallengeService
+    {
+        private readonly ChallengeRepository _challengeRepository;
+        private readonly UserRepository _userRepository;
+        private readonly CategoryRepository _categoryRepository;
+        public ChallengeService(ChallengeRepository challengeRepository, UserRepository userRepository, CategoryRepository categoryRepository)
+        {
+            _challengeRepository = challengeRepository;
+            _userRepository = userRepository;
+            _categoryRepository = categoryRepository;
+        }
+        public async Task CompleteChallengeAsync(int challengeId, int userId)
+        {
+            var existingUser = await _userRepository.GetAsync(userId);
+            if (existingUser == null)
+            {
+                throw new UserNotFoundException(userId);
+            }
+            var existingChallenge = await _challengeRepository.GetAsync(challengeId);
+            if (existingChallenge == null)
+            {
+                throw new Exception($"Challenge with ID {challengeId} doesn't exist.");
+            }
+            if (existingUser.UsersChallenges.Where(uc => uc.ChallengeId == challengeId && uc.State ==StateType.Completed).Any())
+            {
+                throw new Exception($"User with ID {userId} has already completed the challenge with ID {challengeId}.");
+            }
+            if (!existingUser.UsersChallenges.Where(uc => uc.ChallengeId == challengeId).Any())
+            {
+                throw new Exception($"User with ID {userId} hasn't joined the challenge with ID {challengeId}.");
+            }
+
+            await _challengeRepository.CompleteChallengeAsync(existingChallenge, existingUser);
+        }
+
+        public async Task JoinChallengeAsync(int challengeId, int userId)
+        {
+            var existingUser = await _userRepository.GetAsync(userId);
+            if (existingUser == null)
+            {
+                throw new UserNotFoundException(userId);
+            }
+            var existingChallenge = await _challengeRepository.GetAsync(challengeId);
+            if (existingChallenge == null)
+            {
+                throw new Exception($"Challenge with ID {challengeId} doesn't exist.");
+            }
+            if( existingUser.UsersChallenges.Where(uc => uc.ChallengeId == challengeId).Any())
+            {
+                throw new Exception($"User with ID {userId} has already joined the challenge with ID {challengeId}.");
+            }
+
+            await _challengeRepository.JoinChallengeAsync(existingChallenge , existingUser);
+
+        }
+
+        public async Task<List<Challenge>> ShowAllAsync()
+        {
+            return await _challengeRepository.GetAllAsync();
+        }
+
+        public async Task<List<Challenge>> ShowAllByCategoryAsync(int categoryId)
+        {
+            var existingCategory = await _categoryRepository.GetAsync(categoryId);
+            if (existingCategory == null)
+            {
+                throw new Exception($"Category with ID {categoryId} doesn't exist.");
+            }
+            var sortedChallenges = await _challengeRepository.GetAllByCategoryAsync(categoryId);
+            if (sortedChallenges == null || sortedChallenges.Count == 0)
+            {
+                throw new Exception("There are no challenges from this category.");
+            }
+            return sortedChallenges;
+        }
+
+        public async Task<List<Challenge>> ShowAllByLevelAsync(int levelNumber)
+        {
+            var sortedChallenges = await _challengeRepository.GetAllByLevelAsync(levelNumber);
+            if ( sortedChallenges ==null || sortedChallenges.Count ==0 )
+            {
+                throw new Exception("There are no challenges with the specified level number.");
+            }
+            return sortedChallenges;
+        }
+
+        public async Task<List<Challenge>> ShowAllByUserAsync(int userId)
+        {
+            var existingUser = await _userRepository.GetAsync(userId);
+            if (existingUser == null)
+            {
+                throw new UserNotFoundException(userId);
+            }
+            if (existingUser.UsersChallenges == null || !existingUser.UsersChallenges.Any())
+            {
+                throw new Exception($"User with ID {userId} has not joined any challenges.");
+            }
+
+            return await _challengeRepository.GetChallengesByUserAsync(userId);
+        }
+
+        public async Task<List<Challenge>> ShowAllCompletedByUserAsync(int userId)
+        {
+            var existingUser = await _userRepository.GetAsync(userId);
+            if (existingUser == null)
+            {
+                throw new UserNotFoundException(userId);
+            }
+            if (existingUser.UsersChallenges == null || !existingUser.UsersChallenges.Any())
+            {
+                throw new Exception($"User with ID {userId} has not joined any challenges.");
+            }
+            if ( !existingUser.UsersChallenges.Where(uc => uc.State == StateType.Completed ).Any())
+            {
+                throw new Exception($"User with ID {userId} has not completed any challenges.");
+            }
+
+            return await _challengeRepository.GetAllCompletedByUserAsync(userId);
+
+        }
+
+        public async Task<Challenge> ShowAsync(int challengeId)
+        {
+            var existingChallenge = await _challengeRepository.GetAsync(challengeId);
+            if (existingChallenge == null)
+            {
+                throw new Exception($"Challenge with ID {challengeId} not found.");
+            }
+
+            return existingChallenge;
+        }
+    }
+}
